b5418f16a0ed3bf08a3e985cac2ebe8c659805f9
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-06-30 00:37:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  * Description: main -Prints out a string to stdout.
-=======
  * Description: main - Prints out a string to stdout.
->>>>>>> c62b37db2a53048bdd5018d53d98f975d9ecff8f
  * Return: 0 if success.
  */
 int main(void)
 {
-<<<<<<< HEAD
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
-=======
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
->>>>>>> c62b37db2a53048bdd5018d53d98f975d9ecff8f
 }
