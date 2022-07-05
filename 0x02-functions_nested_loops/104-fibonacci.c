4bf892ec3e8a2c724b6d6871e472cf37a09cd17a
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-05 12:53:22 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,7 @@ fibonacci[1] = 2;
 for (a = 2; a < 98; a++)
 {
 fibonacci[a] = fibonacci[a - 1] + fibonacci[a - 2];
-if ((fibonacci[a] % 2 == 0 && fibonacci[a] < 98)
+if (fibonacci[a] % 2 == 0 && fibonacci[a] < 98)
 sum += fibonacci[a];
 }
 printf("%d\n", sum);
