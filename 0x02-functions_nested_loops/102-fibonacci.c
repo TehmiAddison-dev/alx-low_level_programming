5e1f70811a2e8e4972d23ced5b8ddd47078f6e5a
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-05 12:07:32 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,8 +12,8 @@ fibonacci[1] = 2;
 printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 for (a = 2; a < 50; a++)
 {
-fibonacci[a] = fibonacci[a -1] + fibonacci[a - 2];
-if (a ==49)
+fibonacci[a] = fibonacci[a - 1] + fibonacci[a - 2];
+if (a == 49)
 printf("%ld\n", fibonacci[a]);
 else
 printf("%ld, ", fibonacci[a]);
