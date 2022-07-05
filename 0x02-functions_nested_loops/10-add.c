6804f7b6b72cc12e31a60d8c6a2831367c9460a8
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-05 09:40:38 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,10 @@
+#include <stdio.h>
 #include "main.h"
 /**
-* add -adds two numbers together
+* add - adds two numbers together
 * @i: The first number to be added.
 * @j: The second number to be added.
+* Description: Function that adds two numbers and returns the result
 * Result: the sum of two numbers.
 */
 int add(int i, int j)
