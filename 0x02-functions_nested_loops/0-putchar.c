ea1631a141b3c9641ee7a8ae4a7e6bea16a0bb96
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-05 04:59:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,11 @@
 */
 int main(void)
 {
-char _putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-unsigned int i;
-for (i = 0; i < sizeof(_putchar); i++)
+char word[8] ="_putchar";
+int i;
+for (i = 0; i < 8; i++)
 {
-_putchar(_putchar[i]);
+_putchar(word[i]);
 }
 _putchar('\n');
 return (0);
