e201a441fda93b77c5edc58e4b638631b6e3fa1d
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-05 11:12:03 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* @add: Check Main
-* @a: first number to add
-* @b: second number to add
-* Description: Function that adds two numbers and returns the result
-* Result: Always 0 (Success)
+* add - Check Main
+* @a: first integer to add
+* @b: second integer to add
+* Description: Function that adds two integers and returns the result
+* Result: Always 0 (Success).
 */
 int add(int a, int b)
 {
