1595c50a78bcb209d2f369b6c5f822401bd98819
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-04 19:11:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "school.h"
+/**
+* main - prints the english alphabet from a to z.
+* Return: 0 on success.
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
