c3511eb981ed2810842214f0a00fc5114468e015
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-07 06:15:47 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,37 +2,35 @@
 /**
  * print_number - prints a number with _putchar
  * @n: Number to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void print_number(int n)
 {
-	int ten, i, neg;
-	unsigned int digit, digit2;
-
-	ten = 1;
-	if (n < 0)
-	{
-		n = n * -1;
-		neg = 1;
-	}
-	digit = n;
-	digit2 = n;
-	while (digit >= 10)
-	{
-		digit = digit / 10;
-		ten = ten * 10;
-	}
-	if (neg == 1)
-	{
-		_putchar('-');
-	}
-	_putchar('0' + (digit2 / ten));
-	i = ten / 10;
-	while (i >= 1)
-	{
-		_putchar('0' + (digit2 / i) % 10);
-		i = i / 10;
-	}
+int a, i, b;
+unsigned int m, p;
+a = 1;
+if (n < 0)
+{
+n = n * -1;
+b = 1;
+}
+m = n;
+p = n;
+while (m >= 10)
+{
+m = m / 10;
+a = a * 10;
+}
+if (b == 1)
+{
+_putchar('-');
+}
+_putchar('0' + (p / a));
+i = a / 10;
+while (i >= 1)
+{
+_putchar('0' + (p / i) % 10);
+i = i / 10;
+}
 }
