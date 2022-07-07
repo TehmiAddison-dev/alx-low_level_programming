5e1e2525f458cdc0e8dc72c36f3ee6ea78ec6dea
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-07 04:56:07 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,19 +7,18 @@
 */
 void print_triangle(int size)
 {
-int i = o, j, n = size - 1;
+int i, j;
 if (size > 0)
 {
-for (; i < size; i++)
+for (i = 1; i <= size; i++)
 {
-for (j = 0; j < size; j++)
+for (j = size; j >= 1; j--)
 {
-if (j < n)
+if (i < j)
 _putchar(' ');
 else
 _putchar('#');
 }
-n--;
 _putchar('\n');
 }
 }
