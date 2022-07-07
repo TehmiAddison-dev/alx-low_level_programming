6620304636b128dc63afb6dc3d0b1ef82167671b
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-07 04:50:34 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,17 +2,17 @@
 /**
 * print_square - function that prints a square,
 * followed by a new line.
-* @size: an input integer
+* @n: an input integer
 * Return: Always 0
 */
-void print_square(int size)
+void print_square(int n)
 {
 int i, j;
-if (size > 0)
+if (n > 0)
 {
-for (i = 0; i < size; i++)
+for (i = 0; i < n; i++)
 {
-for (j = 0; j < size; j++)
+for (j = 0; j < n; j++)
 _putchar('#');
 _putchar('\n');
 }
