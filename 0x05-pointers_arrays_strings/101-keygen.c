c97d0a2f1334db206187ecc413b40c9b5fa927ea
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-12 08:10:31 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,45 +7,38 @@
 */
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
-
-	srand(time(0));
-
-	while (sum < 2772)
-	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
-
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
-		}
-	}
-
-	printf("%s", password);
-
-	return (0);
+char password[84];
+ int a = 0, b = 0, x, y;
+srand(time(0));
+while (b < 2772)
+{
+password[a] = 33 + rand() % 94;
+b += password[a++];
+}
+password[a] = '\0';
+if (b != 2772)
+{
+x = (b - 2772) / 2;
+y = (b - 2772) / 2;
+if ((b - 2772) % 2 != 0)
+x++;
+for (a = 0; password[a]; a++)
+{
+if (password[a] >= (33 + x))
+{
+password[index] -= x;
+break;
+}
+}
+for (a = 0; password[a]; a++)
+{
+if (password[a] >= (33 + y))
+{
+password[a] -= y;
+break;
+}
+}
+}
+printf("%s", password);
+return (0);
 }
