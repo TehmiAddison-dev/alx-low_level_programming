a707187ff79bba76b79826adeabfcf3479fe6149
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-07-12 15:55:43 +0100

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,21 +7,45 @@
 */
 int main(void)
 {
-int a;
-int b = 0;
-int randnum;
-char p[100];
-srand(time(NULL));
-for (a = 0; a < 100; a++)
-{
-randnum = random() % 127;
-if (randnum > 32)
-{
-p[b] = randnum;
-b++;
-}
-}
-p[b] = '\0';
-printf("%s", p);
-return (0);
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
+
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
+	return (0);
 }
