c3f661c52f553138871e4a9f9c92bedc2a339564
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-12 01:01:46 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - returns the length of a string.
+* @c: input a word'
+* Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int large = 0;
+while (*(c + large) != '\0')
+{
+large++;
+}
+return (large);
+}
