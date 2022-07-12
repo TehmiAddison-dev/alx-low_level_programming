d43ac708b3784a729a3e422d9cf4756bd97469b0
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-12 06:20:30 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* * _strcpy - copies the string pointed to by sr,
+* @dest: variable holding the string.
+* @src: variable holding the string.
+* Return: Always 0.
+*/
+char *_strcpy(char *dest, char *src)
+{
+int a = 0;
+while (src[a] != '\0')
+{
+dest[a] = src[a];
+a++;
+}
+dest[a] = '\0';
+return (dest);
+}
+ 
