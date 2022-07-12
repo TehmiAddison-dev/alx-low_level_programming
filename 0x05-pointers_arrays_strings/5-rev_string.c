9acb952a134b179985d37151a68d3c68568c8c7e
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-12 02:54:52 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* rev_string - reverses a string.
+* @s: input variable.
+* Return: Always 0.
+*/
+void rev_string(char *s)
+{
+int i;
+int size = 0;
+char rev[10000];
+while (s[size] != '\0')
+{
+size++;
+}
+size--;
+for (i = 0; i <=size; i++)
+{
+rev[i] = s[size - i];
+}
+for (i = 0; i <= size; i++)
+{
+s[i] = rev[i];
+}
+}
