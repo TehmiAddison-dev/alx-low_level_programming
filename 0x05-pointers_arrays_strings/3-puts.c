609e43464d639d08bf33c20eada3928b7b653e60
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-12 01:39:04 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,10 +6,11 @@
 */
 void _puts(char *str)
 {
-while (*str != '\0')
+int i = 0;
+while (str[i] != '\0')
 {
-_putchar(*str);
-str++;
-}
-_putchar('\n'); 
+  _putchar(str[i]);
+  i++;
+ }
+ _putchar('\n');
 }
