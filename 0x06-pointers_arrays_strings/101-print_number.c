74f1b80f937652b19d59fbf50aca40614811d645
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-14 01:12:03 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_number - prints an integer.
+* @n: input integer.
+* Return: Always 0.
+*/
+void print_number(int n)
+{
+int res, temp, expo;
+expo = 1;
+if (n >= 0)
+res = n * -1;
+else
+{
+res = n;
+_putchar('-');
+}
+temp = res;
+while (temp <= -10)
+{
+expo *= 10;
+temp /= 10;
+}
+while (expo >= 1)
+{
+_putchar((res / expo) * -1 + '0');
+expo /= 10;
+}
+}
