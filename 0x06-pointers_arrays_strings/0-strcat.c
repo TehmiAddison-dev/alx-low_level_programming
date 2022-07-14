cab756449fd5a0a599b6f9d9b4a9701a676b1673
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-07-14 22:51:45 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,16 @@
 */
 char *_strcat(char *dest, char *src)
 {
-char *temp = dest;
-while (*dest != '\0')
-{
-dest++;
-}
-while (*src != '\0')
-{
-*dest = *src;
-dest++;
-src++;
-}
-*dest = '\0';
-return (temp); 
+	int x = 0;
+	int y = 0;
+
+	while (dest[x] != '\0')
+		x++;
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		y++;
+		x++;
+	}
+	return (dest);
 }
