5ea595e6698055813d9d60bcd0ce51a19f8166d7
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-13 22:54:12 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* *_strcat - concatenates two strings
+* @src: src parameter.
+* @dest: parameter to append. 
+* Return: Always 0.
+*/
+char *_strcat(char *dest, char *src)
+{
+int x = 0, int y = 0;
+while (dest[x] != '\0')
+x++;
+while (src[y]!= '\0')
+{
+dest[x] = src[y];
+y++;
+x++;
+}
+return (dest); 
+}
