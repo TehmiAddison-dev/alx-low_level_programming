a4d11c922fecd96b3639863d7288216f46d4cd43
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-18 08:11:39 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,26 +6,24 @@
 * @needle: an input string to locate into string haystack
 * Return: NULL
 */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	int j;
-	int z;
-
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (z = i, j = 0; needle[j] != '\0'; j++, z++)
-		{
-			if (haystack[z] != needle[j] || haystack[z] == '\0')
-			{
-				break;
-			}
-		}
-		if (needle[j] == '\0')
-		{
-			return (haystack + i);
-		}
-	}
-	return (NULL);
+int i;
+int j;
+int z;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (z = i, j = 0; needle[j] != '\0'; j++, z++)
+{
+if (haystack[z] != needle[j] || haystack[z] == '\0')
+{
+break;
+}
+}
+if (needle[j] == '\0')
+{
+return (haystack + i);
+}
+}
+return (NULL);
 }
