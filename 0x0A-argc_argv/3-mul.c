5b84bfc309ed19b76f5a23fc600d785e28f383fa
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-07-22 12:51:10 +0100

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,19 +6,20 @@
 * @argv: argument vector of pointers to strings
 * Return: Always 0.
 */
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-int i, j;
-i = j = 0;
-if (argc == 3)
+int i, j, n;
+if (argc != 3)
 {
-i = atoi(argv[1]);
-j = atoi(argv[2])
-printf("%d\n", i * j);
-return (0);
-}
-else
 printf("Error\n");
 return (1);
 }
-
+else
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+n = i * j;
+printf("%d\n", n);
+return (0);
+}
+}
