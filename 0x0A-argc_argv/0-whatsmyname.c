bbb8ea3abb541bef280c06c29fe4d4d55d3560b7
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-22 04:18:17 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
 * @argv: arguent vector of pointers to strings
 * Return: Always 0
 */
-int main(int arg _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 argc--; /*added to pass checks*/
 printf("%s\n", argv[0]);
