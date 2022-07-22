ee17d29c8c3dd6e0109f3dba159ddc9faba26154
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-07-22 13:06:56 +0100

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,32 +6,41 @@
 * @argv: argument vector of pointers to strings
 * Return: 0 if no errors, else 1
 */
+
 int main(int argc, char *argv[])
 {
-int a, n = 0, i, t;
-int c[5] = {25, 10, 5, 2, 1};
+int cents, coins = 0;
 if (argc != 2)
 {
-puts("Error");
+printf("Error\n");
 return (1);
 }
-a = atoi(argv[1]);
-if (a <= 0)
+cents = atoi(argv[1]);
+while (cents > 0)
 {
-puts("0");
-return (1);
-}
-else
+coins++;
+if ((cents - 25) >= 0)
 {
-for (i = 0; i < 5; i++)
+cents -= 25;
+continue;
+}
+if ((cents - 10) >= 0)
 {
-t = a / c[i];
-a -= t * c[i];
-n += t;
-if (a == 0)
-break;
+cents -= 10;
+continue;
 }
+if ((cents - 5) >= 0)
+{
+cents -= 5;
+continue;
 }
-printf("%d\n", n);
+if ((cents - 2) >= 0)
+{
+cents -= 2;
+continue;
+}
+cents--;
+}
+printf("%d\n", coins);
 return (0);
 }
