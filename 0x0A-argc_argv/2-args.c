3ccd176135e5c9aad62261767e0f75f5cc59dfd3
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-22 03:35:07 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints all arguments it receives
+* @argc: number of arguments passed to the function
+* @argv: argument vector of pointers to strings
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+int a;
+for (a = 0; a < argc; a++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
