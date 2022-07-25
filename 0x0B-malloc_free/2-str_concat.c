e532221bdcabb3c771c36f6e1399bb35be4a3294
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-07-25 05:44:34 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
-* _strlen - length of a string
-* @s: input char
-* Return: length of a string
+* str_concat - Concatenates two strings of any size
+* @s1: the first string to concatenate
+* @s2: the second string to concatenate
+* Return: the two strings concatenated
 */
 
-int _strlen(char *s)
-{
-int l = 0;
-while (*s != '\0')
-{
-s++;
-l++;
-}
-return (l);
-}
-/**
-* str_concat - Concat 2 strings.
-* @s1: string
-* @s2: string
-* Return: char
-*/
 char *str_concat(char *s1, char *s2)
 {
-unsigned int l1, l2;
-char *conc, *tmp;
-if (!s1)
+int i = 0, j = 0, k = 0, l = 0;
+char *s;
+if (s1 == NULL)
 s1 = "";
-else
-l1 = _strlen(s1);
-if (!s2)
+if (s2 == NULL)
 s2 = "";
-else
-l2 = _strlen(s2);
-conc = malloc(l1 + l2 + 1);
-if (!conc)
-return (0);
-tmp = conc;
-while (*s1)
-*tmp++ = *s1++;
-while ((*tmp++ = *s2++))
-;
-return (conc);
+while (s1[i])
+i++;
+while (s2[j])
+j++;
+l = i + j;
+s = malloc((sizeof(char) * l) + 1);
+if (s == NULL)
+return (NULL);
+j = 0;
+while (k < l)
+{
+if (k <= i)
+s[k] = s1[k];
+if (k >= i)
+{
+s[k] = s2[j];
+j++;
+}
+k++;
+}
+s[k] = '\0';
+return (s);
 }
