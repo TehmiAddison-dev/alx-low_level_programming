b4fd553ec4d643c72f4e854932808a55476cdd9e
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-28 04:51:04 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* malloc_checked - allocates memory using malloc
+* @b: size of buffer
+* Return: pointer to buffer
+*/
+void *malloc_checked(unsined int b);
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p); 
+}
