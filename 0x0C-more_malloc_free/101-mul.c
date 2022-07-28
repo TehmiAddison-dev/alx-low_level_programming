0aff0bbfa0e0a7649af8441116704a44f4024d5d
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-07-28 05:56:20 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,7 +8,7 @@
 */
 int main(int argc, char **argv)
 {
-int num1, num2, result;
+int num1, num2, mul;
 if (argc != 3)
 {
 printf("Error\n");
@@ -18,8 +18,8 @@ else
 {
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
-printf("%d\n", result);
+mul = num1 * num2;
+printf("%d\n", mul);
 return (0);
 }
 }
