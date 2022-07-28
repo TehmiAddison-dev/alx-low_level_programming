ae7f3a94b2381e44431d1d2eace5fc7268d710d1
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-07-28 15:26:15 +0100

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,6 +7,7 @@
 * @c: the character to check
 * Return: 1 if digit, 0 otherwise
 */
+
 int _isdigit(int c)
 {
 return (c >= '0' && c <= '9');
@@ -14,6 +15,7 @@ return (c >= '0' && c <= '9');
 /**
 * _strlen - returns the length of a string
 * @s: the string whose length to check
+*
 * Return: integer length of string
 */
 int _strlen(char *s)
