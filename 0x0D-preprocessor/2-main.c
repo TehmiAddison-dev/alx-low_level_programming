a893617a0e0823316a422edaae1b855bc19daf41
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-08-01 21:58:38 +0100

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - prints the name of the file of a program
+* main - rints the name of the file of a program
 * Return: 0.
 */
 int main(void)
 {
-printf("%s\n", _FILE_);
+printf("%s\n", __FILE__);
 return (0);
 }
