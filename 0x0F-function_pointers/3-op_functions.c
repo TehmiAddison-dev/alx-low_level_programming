2d152d42ef684e3dcd0b13ac59ac2ab549e552e2
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-08-03 22:38:53 +0100

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,5 @@
-#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
 * op_add - adds two numbers
 * @a: first number
@@ -37,6 +38,11 @@ return (a * b)
 */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b)
 }
 /**
@@ -47,5 +53,10 @@ return (a / b)
 */
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b)
 }
