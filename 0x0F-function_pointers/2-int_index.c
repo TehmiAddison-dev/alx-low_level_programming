1c981de9a53f103f1d319114c0495a2cd9e6b430
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-08-03 14:24:36 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,8 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i;
 i = -1;
 if (size <= 0 || !(array) || !(cmp))
-
-  return (-1);
+return (-1);
 while (++i < size)
 {
 if (cmp(array[i]))
