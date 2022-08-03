97977727d626a8e5f61d189e9ea07c4a9cf09652
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-08-03 05:12:17 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,14 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
 * print_name - prints a name
 * @name: input string
 * @f: function pointer casted to char pointer
+* Return: Nothing
 */
 void print_name(char *name, void (*f)(char *))
 {
-(*f)(name);
+if (name == NULL || f == NULL)
+return;
+f(name);
 }
