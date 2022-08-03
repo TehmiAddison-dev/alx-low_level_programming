0f9268b1a7c4759109982267170e389a2cb5a6e8
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-08-03 22:35:16 +0100

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
 * main - program to perform simple operations
 * @argc: argument count
@@ -10,17 +9,26 @@
 */
 int main(int argc, char *argv[])
 {
-int (*func)(int, int);
+int a, b;
+int (*o)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-func = get_op_func(argv[2]);
-if (func == NULL)
+if (argv[2][1] != '\0')
 {
 printf("Error\n")
 exit(99);
-}printf("%d\n", func(atoi(argv[1], atoi(argv[3])));
+}
+o = get_op_func(argv[2]);
+if (o == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+printf("%d\n", o(a, b));
 return (0);
 }
