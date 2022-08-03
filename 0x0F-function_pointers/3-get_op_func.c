423bd1d571b04a2c176ac7c4d5da50e29ee6c635
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-08-03 22:55:19 +0100

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,22 +7,25 @@
 * Return: result of the operation
 */
 int (*get_op_func(char *s))(int, int)
-{
 op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i;
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
 i = 0;
-while (i < 5)
-{
-if (*ops[i].op == *s && !(*(s + 1)))
-return (ops[i].f);
-i++;
+while (ops[i].op)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
 }
-return (NULL);
-}
+
