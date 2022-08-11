3ca134db0a8b0b862dd9911878286b94fa920f2e
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-08-11 15:34:46 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,6 @@
 * @str: pointer to the str to be added to the node
 * Return: the address of the new element, or NULL if it failed
 */
-
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node = malloc(sizeof(list_t));
