347c87d8e718b8d6bb0d1af4ab38f0abc644a429
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-08-11 14:30:59 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,17 +1,6 @@
-#include "lists.h"
 #include <stdlib.h>
-/**
-* _strlen - gets length of the string
-* @s: string
-* Return: length of the string
-*/
-int _strlen(const char *s)
-{
-int i;
-for (i = 0; s[i]; i++)
-;
-return (i);
-}
+#include <string.h>
+#include "lists.h"
 /**
 * add_node - add new nodes to the list
 * @head: current place in the list
@@ -20,27 +9,16 @@ return (i);
 */
 list_t *add_node(list_t **head, const char *str)
 {
-int i, len;
-char *content;
 list_t *new;
-if (str == NULL || head == NULL)
-return (NULL);
-len = _strlen(str);
-new = *head;
-content = malloc((len + 1) * sizeof(char));
-if (content == NULL)
-return (NULL);
-for (i = 0; str[i]; i++)
-content[i] = str[i];
+unsigned int len = 0;
+while (str[len])
+len++;
 new = malloc(sizeof(list_t));
-if (new == NULL)
-{
-free(content);
+if (!new)
 return (NULL);
-}
-new->str = content;
+new->str = strdup(ytr);
 new->len = len;
-new->next = *head;
-*head = new;
-return (new);
+new->next = (*head);
+(*head) = new;
+return (*head);
 }
