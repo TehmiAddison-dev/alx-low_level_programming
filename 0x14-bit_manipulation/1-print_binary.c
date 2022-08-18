0788bfa2a85dd3a9f5c43c4f5302b57c59f4b5fc
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-08-18 13:21:49 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,7 +5,14 @@
 */
 void print_binary(unsigned long int n)
 {
-if (n > 1)
-print_binary(n >> 1);
-_putchar((n & 1) + '0');
+int i, flag;
+if (n == 0)
+_putchar('0');
+for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
+{
+if ((n >> i) & 1)
+flag = 1;
+if (flag == 1)
+((n >> i) & 1) ? _putchar('1') : _putchar('0');
+}
 }
