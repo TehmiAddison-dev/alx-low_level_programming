0a35ca0bd59a56cd079e5d2682cfd6cef2ef22cd
Temitope Ashaolu <temitopeashaolu@yahoo.co.uk>
2022-08-25 06:23:18 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 /**
-  * placeholder2 - define function
-  * @void: describe argument
-  * Return: 0 on success
-  */
+* placeholder2 - define function
+* @void: describe argument
+* Return: 0 on success
+*/
 int placeholder2(void)
 {
-	return (0);
+return (0);
+}
