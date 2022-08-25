ea7713d4690b65ae5f9ac5728d434a8b572c8b60
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-08-25 14:07:14 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,26 @@
 #include "main.h"
-
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- * Otherwise - 1.
- */
-
+* create_file - creates a file with rw------- permissions
+* @filename: name of the file, if NULL, return -1
+* @text_content: contents of the file. If NULL, create an empty file
+* Return: 1 on success, -1 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
-
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
-	return (1);
+int new_file, len, wr_stat;
+if (filename == NULL)
+return (-1);
+new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (new_file == -1)
+return (-1);
+if (text_content == NULL)
+{
+close(new_file);
+return (1);
+}
+for (len = 0; text_content[len]; len++)
+;
+wr_stat = write(new_file, text_content, len);
+if (close(new_file) == -1)
+return (-1);
+return (wr_stat == -1 ? -1 : 1);
