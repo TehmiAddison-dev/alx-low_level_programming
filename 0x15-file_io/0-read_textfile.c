9617a9b2e959924acc59626211766d88a668e56c
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-08-25 13:55:49 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
 * @filename: name of file to read
 * @letters: number of characters to print
 * Return: 0 on success
-**/
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 ssize_t o, r, w;
