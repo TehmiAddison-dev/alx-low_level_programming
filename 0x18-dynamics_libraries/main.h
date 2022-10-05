5e1348d40648a6b5ce9962ba16da195abe3fda7e
ASHAOLU Temitope <temitopeashaolu@yahoo.co.uk>
2022-10-05 12:00:10 +0100

    main.h

--- 0x18-dynamics_libraries/main.h
+++ 0x18-dynamics_libraries/main.h
@@ -9,7 +9,6 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-int mul(int argc, char *argv[]);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -22,4 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+#endif /* MAIN_H */
